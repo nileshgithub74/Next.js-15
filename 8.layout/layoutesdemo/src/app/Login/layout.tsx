3aeb6fa0423deec34bd23b19dd8c91581f894034
Nilesh kumar <kumarnilesh843127@gmail.com>
2025-03-30 01:24:45 +0530

    next

---
+++ 8.layout/layoutesdemo/src/app/Login/layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/">Login Main</Link>
+        </li>
+        <li>
+          <Link href="/login/loginuser">Login Regular User</Link>
+        </li>
+        <li>
+          <Link href="/login/loginadmin">Login Admin</Link>
+        </li>
+      </ul>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
